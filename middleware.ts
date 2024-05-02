a1e33fcfdeded1aa58a8de9385c1d6f0bebb80ff
Abhay <abhay.singh3104@gmail.com>
2024-05-02 22:23:04 +0530

    c

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk']
-});
+// export default authMiddleware({
+//   publicRoutes: ['/api/webhooks/clerk']
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
