879ef1757261d03d30b28071de6326495a37d391
Abhay <abhay.singh3104@gmail.com>
2024-05-10 07:07:30 +0530

    a

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,6 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-  publicRoutes: ['/api/webhooks/clerk'],
+  // publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
 };
